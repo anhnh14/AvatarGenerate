9bed279f92def952a77d060c50b3f6b901dbc27d
Hoang Nguyen <hnguyen@thenewoffice.com>
2019-08-02 16:31:53 -0500

    Initial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,51 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex:  [r, g, b | _tail]} = image
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+
+  end
+
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+  end
+
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+    hex
+    |> Enum.chunk_every(3)
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
