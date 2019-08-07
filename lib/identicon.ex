19d82c2548a0972ee44d7c70d6a94fa23c13274a
Hoang Nguyen <hnguyen@thenewoffice.com>
2019-08-07 17:42:14 -0500

    Add method generate pixel

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -27,6 +27,22 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(image) do
+    %Identicon.Image{grid: grid} = image
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_righ = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_righ}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(image) do
